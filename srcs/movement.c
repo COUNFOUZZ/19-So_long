0076ab1e7c880e39cbbe621af7c02294174f79f5
Alexis Abda <alexis.abda@hotmail.com>
2022-12-17 15:52:40 +0100

    Add ft_save_last_pos function and apply the function (NEED TO BE NORMED)

--- srcs/movement.c
+++ srcs/movement.c
@@ -6,11 +6,43 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 23:54:56 by aabda             #+#    #+#             */
-/*   Updated: 2022/12/06 12:26:41 by aabda            ###   ########.fr       */
+/*   Updated: 2022/12/17 15:50:32 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
+
+static void	ft_save_last_pos(char key, t_game *g)
+{
+	if (key == 'W')
+	{
+		if (g->map.map[g->player.pos_y - 1][g->player.pos_x] != 'C')
+			g->player.char_staged = '0';
+		if (g->map.map[g->player.pos_y][g->player.pos_x] == g->map.map[g->check.exit_pos_y][g->check.exit_pos_x])
+			g->player.char_staged = 'E';
+	}
+	else if (key == 'S')
+	{
+		if (g->map.map[g->player.pos_y - 1][g->player.pos_x] != 'C')
+			g->player.char_staged = '0';
+		if (g->map.map[g->player.pos_y][g->player.pos_x] == g->map.map[g->check.exit_pos_y][g->check.exit_pos_x])
+			g->player.char_staged = 'E';
+	}
+	else if (key == 'D')
+	{
+		if (g->map.map[g->player.pos_y][g->player.pos_x + 1] != 'C')
+			g->player.char_staged = '0';
+		if (g->map.map[g->player.pos_y][g->player.pos_x] == g->map.map[g->check.exit_pos_y][g->check.exit_pos_x])
+			g->player.char_staged = 'E';
+	}
+	else if (key == 'A')
+	{
+		if (g->map.map[g->player.pos_y][g->player.pos_x - 1] != 'C')
+			g->player.char_staged = '0';
+		if (g->map.map[g->player.pos_y][g->player.pos_x] == g->map.map[g->check.exit_pos_y][g->check.exit_pos_x])
+			g->player.char_staged = 'E';
+	}
+}
 
 static void	ft_exit_and_coin_collect(t_game *g)
 {
@@ -24,39 +56,47 @@ static void	ft_exit_and_coin_collect(t_game *g)
 			g->player.coin_collected++;
 }
 
-static void	ft_movement_y(char move, t_game *g)
+static void	ft_movement_y(char key, t_game *g)
 {
-	if (move == 'W')
+	if (key == 'W')
 	{
+		ft_save_last_pos('W', g);
 		g->player.pos_y--;
 		ft_exit_and_coin_collect(g);
 		g->map.map[g->player.pos_y][g->player.pos_x] = 'P';
-		g->map.map[g->player.pos_y + 1][g->player.pos_x] = '0';
+		g->map.map[g->player.pos_y + 1][g->player.pos_x] = g->player.char_staged;
+		g->player.move++;
 	}
-	else if (move == 'S')
+	else if (key == 'S')
 	{
+		ft_save_last_pos('S', g);
 		g->player.pos_y++;
 		ft_exit_and_coin_collect(g);
 		g->map.map[g->player.pos_y][g->player.pos_x] = 'P';
-		g->map.map[g->player.pos_y - 1][g->player.pos_x] = '0';
+		g->map.map[g->player.pos_y - 1][g->player.pos_x] = g->player.char_staged;
+		g->player.move++;
 	}
 }
 
-static void	ft_movement_x(char move, t_game *g)
+static void	ft_movement_x(char key, t_game *g)
 {
-	if (move == 'D')
+	if (key == 'D')
 	{
+		ft_save_last_pos('D', g);
 		g->player.pos_x++;
 		ft_exit_and_coin_collect(g);
 		g->map.map[g->player.pos_y][g->player.pos_x] = 'P';
-		g->map.map[g->player.pos_y][g->player.pos_x - 1] = '0';
+		g->map.map[g->player.pos_y][g->player.pos_x - 1] = g->player.char_staged;
+		g->player.move++;
 	}
-	else if (move == 'A')
+	else if (key == 'A')
 	{
+		ft_save_last_pos('A', g);
 		g->player.pos_x--;
 		ft_exit_and_coin_collect(g);
 		g->map.map[g->player.pos_y][g->player.pos_x] = 'P';
-		g->map.map[g->player.pos_y][g->player.pos_x + 1] = '0';
+		g->map.map[g->player.pos_y][g->player.pos_x + 1] = g->player.char_staged;
+		g->player.move++;
 	}
 }
 
