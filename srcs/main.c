6c0abcadd399512e22df571c3df2eff50e97797e
Alexis Abda <alexis.abda@hotmail.com>
2022-12-17 15:54:11 +0100

    Update main with some debug and change EVENT_KEY_RELEASE to EVENT_KEY_PRESS

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 21:31:17 by aabda             #+#    #+#             */
-/*   Updated: 2022/12/06 11:03:05 by aabda            ###   ########.fr       */
+/*   Updated: 2022/12/17 15:22:00 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,12 @@ int	main(int argc, const char **argv)
 	{
 		ft_path_map("maps/", argv[1], &g);
 		init_struct(&g);
+		printf("\nMap_max_size:\n");
 		printf("max_y = %d\tmax_x = %d\n", g.map.nl, g.map.lenline);
 		// printf("%s", g.map.map[5]);
 		// printf("%c\n", g.map.map[5][g.map.lenline - 2]);
 		mlx_hook(g.mlx_win, 17, 0, &red_cross, &g);
-		mlx_hook(g.mlx_win, X_EVENT_KEY_RELEASE, 0, &key_pressed, &g);
+		mlx_hook(g.mlx_win, X_EVENT_KEY_PRESS, 0, &key_pressed, &g);
 		mlx_loop(g.mlx);
 		ft_free_tab(NULL, g.map.map);
 		// system("leaks so_long");
