e3b15ac1c5f5c5809dfa366b1da9ba9076d52c57
Alexis Abda <alexis.abda@hotmail.com>
2022-12-19 20:26:36 +0100

    add color in the debug

--- srcs/mlx_utils.c
+++ srcs/mlx_utils.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:45:25 by aabda             #+#    #+#             */
-/*   Updated: 2022/12/17 15:24:12 by aabda            ###   ########.fr       */
+/*   Updated: 2022/12/18 21:48:09 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,8 @@ int	key_pressed(int keycode, t_game *g)
 	ft_movement(keycode, g);
 	system("clear");
 	ft_print_map1(g);
-	printf("coin collected : %d\n", g->player.coin_collected);
-	printf("Nbr_move:\n");
-	printf("moves = %d\n", g->player.move);
+	printf("Coin collected : " YELLOW "%d\n" NORMAL, g->player.coin_collected);
+	printf("Nbr_move : %d\n", g->player.move);
 	ft_img(g, "./img/floor.xpm", &(g->img).img_width, &(g->img).img_height);
 	mlx_clear_window(g->mlx, g->mlx_win);
 	mlx_put_image_to_window(g->mlx, g->mlx_win, g->img.img, g->player.pos_x, g->player.pos_y);
