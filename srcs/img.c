2bc4a060ac817513c400cf077532e29904708647
Alexis Abda <alexis.abda@hotmail.com>
2023-01-03 18:38:56 +0100

    Add if an image if the player is on the exit and he don't have collected all the coin

--- srcs/img.c
+++ srcs/img.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 14:06:19 by aabda             #+#    #+#             */
-/*   Updated: 2023/01/03 17:23:30 by aabda            ###   ########.fr       */
+/*   Updated: 2023/01/03 18:35:25 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,16 @@ void	ft_init_img(t_game *g)
 	g->img.wall = mlx_xpm_file_to_image(g->mlx, "./img/wall.xpm", &g->img.img_width, &g->img.img_height);
 	g->img.floor = mlx_xpm_file_to_image(g->mlx, "./img/floor.xpm", &g->img.img_width, &g->img.img_height);
 	g->img.player_floor = mlx_xpm_file_to_image(g->mlx, "./img/player_floor.xpm", &g->img.img_width, &g->img.img_height);
+	g->img.player_exit = mlx_xpm_file_to_image(g->mlx, "./img/player_exit.xpm", &g->img.img_width, &g->img.img_height);
 	g->img.coin = mlx_xpm_file_to_image(g->mlx, "./img/coin.xpm", &g->img.img_width, &g->img.img_height);
 	g->img.dclose = mlx_xpm_file_to_image(g->mlx, "./img/dclose.xpm", &g->img.img_width, &g->img.img_height);
-	g->img.dclose = mlx_xpm_file_to_image(g->mlx, "./img/dclose.xpm", &g->img.img_width, &g->img.img_height);
+	g->img.dopen = mlx_xpm_file_to_image(g->mlx, "./img/dopen.xpm", &g->img.img_width, &g->img.img_height);
 	if (!g->img.coin || !g->img.dclose || !g->img.dopen || !g->img.floor
 		|| !g->img.player_exit || !g->img.player_floor || !g->img.wall)
 		ft_error(-1, NULL, NULL, ERR_INIT_IMG);
 }
 
-static void	ft_put_img(t_game *g, int x, int y, const char c)
+void	ft_put_img(t_game *g, int x, int y, const char c)
 {
 	if (c == '1')
 		mlx_put_image_to_window(g->mlx, g->mlx_win, g->img.wall, y * 32, x * 32);
@@ -38,7 +39,9 @@ static void	ft_put_img(t_game *g, int x, int y, const char c)
 	if (c == 'E')
 		mlx_put_image_to_window(g->mlx, g->mlx_win, g->img.dclose, y * 32, x * 32);
 	if (c == 'e')
-		mlx_put_image_to_window(g->mlx, g->mlx_win, g->img.dclose, y * 32, x * 32);
+		mlx_put_image_to_window(g->mlx, g->mlx_win, g->img.dopen, y * 32, x * 32);
+	if (c == 'p')
+		mlx_put_image_to_window(g->mlx, g->mlx_win, g->img.player_exit, y * 32, x * 32);
 }
 
 void	ft_put_img_map(t_game *g)
@@ -56,10 +59,14 @@ void	ft_put_img_map(t_game *g)
 				ft_put_img(g, i, j, '1');
 			else if (g->map.map[i][j] == '0')
 				ft_put_img(g, i, j, '0');
+			else if (g->map.map[i][j] == 'P' && g->player.pos_y == g->check.exit_pos_y && g->player.pos_x == g->check.exit_pos_x && g->check.nbr_coin != g->player.coin_collected)
+				ft_put_img(g, i, j, 'p');
 			else if (g->map.map[i][j] == 'P')
 				ft_put_img(g, i, j, 'P');
 			else if (g->map.map[i][j] == 'C')
 				ft_put_img(g, i, j, 'C');
+			else if (g->map.map[i][j] == 'E' && g->check.nbr_coin == g->player.coin_collected)
+				ft_put_img(g, i, j, 'e');
 			else if (g->map.map[i][j] == 'E')
 				ft_put_img(g, i, j, 'E');
 		}
