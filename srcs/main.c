9c18bffe0bdcc8232a7a6a3abbcff0fcfdacf837
Alexis Abda <alexis.abda@hotmail.com>
2023-01-03 17:29:20 +0100

    Update main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 21:31:17 by aabda             #+#    #+#             */
-/*   Updated: 2022/12/29 17:16:39 by aabda            ###   ########.fr       */
+/*   Updated: 2023/01/03 17:23:42 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,17 +55,12 @@ int	main(int argc, const char **argv)
 	{
 		ft_parse_argv(&g, (char *)argv[1]);
 		init_struct(&g);
-		printf("\nMap_max_size:\n");
-		printf("max_y = %d\tmax_x = %d\n", g.map.nl, g.map.lenline);
-		// printf("%s", g.map.map[5]);
-		// printf("%c\n", g.map.map[5][g.map.lenline - 2]);
 		ft_print_map1(&g);
-		// printf("size struct :%lu\n", sizeof(t_game*));
+		ft_put_img_map(&g);
 		mlx_hook(g.mlx_win, 17, 0, &red_cross, &g);
 		mlx_hook(g.mlx_win, X_EVENT_KEY_PRESS, 0, &key_pressed, &g);
 		mlx_loop(g.mlx);
 		ft_free_tab(NULL, g.map.map);
-		// system("leaks so_long");
 	}
 	else
 		ft_error(-1, NULL, NULL, ERR_BAD_ARG);
