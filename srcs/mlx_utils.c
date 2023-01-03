c90c62d9856c373810fa65cb8791d00ec93024e9
Alexis Abda <alexis.abda@hotmail.com>
2023-01-03 18:40:19 +0100

    Add function mlx_clear_window and ft_put_img_map in key_pressed function

--- srcs/mlx_utils.c
+++ srcs/mlx_utils.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:45:25 by aabda             #+#    #+#             */
-/*   Updated: 2023/01/03 14:57:11 by aabda            ###   ########.fr       */
+/*   Updated: 2023/01/03 17:49:51 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ int	red_cross(void)
 
 int	key_pressed(int keycode, t_game *g)
 {
+	mlx_clear_window(g->mlx, g->mlx_win);
 	ft_movement(keycode, g);
+	ft_put_img_map(g);
 	system("clear");
 	ft_print_map1(g);
 	printf("Coin collected : " YELLOW "%d\n" NORMAL, g->player.coin_collected);
